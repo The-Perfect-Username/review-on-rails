51e9a1ec71df2ca771ae54bf05255d7b3c25b179
Lachlan Enderlin <lendo1995@gmail.com>
2017-05-05 12:11:58 +1000

    Added password encryption

--- dist/review-on-rails/db/schema.rb
+++ dist/review-on-rails/db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504055420) do
+ActiveRecord::Schema.define(version: 20170505020823) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
     t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
